950a662abb5b26b6991fcf5e598942d743a2cc4e
jorisguffens <joris.guffens@outlook.com>
2022-07-13 11:03:34 +0000

    Deploying to javadocs from @ GufliMC/BrickHolograms@051b4d3e9233256fa13f77e075aa6d8fb37cab9a 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.guflimc.brick.holograms.api"},{"l":"com.guflimc.brick.holograms.api.domain"},{"l":"com.guflimc.brick.holograms.api.meta"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.guflimc.brick.holograms.api"},{"l":"com.guflimc.brick.holograms.api.domain"}];updateSearchResults();
